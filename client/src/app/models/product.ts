34439dc2bd6722774dab6cdd5fcd4aba0a36e549
DESKTOP-4SFC0M1\HP <toannvph25194@fpt.edu.vn>
2024-02-21 11:16:07 +0700

    End of section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  pictureUrl: string;
+  producttype: string;
+  producttypeId: number;
+  productbrand: string;
+  productbrandId: number;
+}
