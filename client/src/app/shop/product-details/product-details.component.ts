c1a99b4df57768d7490c87dc1ca3f4ca76c86cc7
DESKTOP-4SFC0M1\HP <toannvph25194@fpt.edu.vn>
2024-03-05 22:54:47 +0700

    End of section 12

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { Breadcrumb } from 'xng-breadcrumb/lib/types/breadcrumb';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +14,12 @@ export class ProductDetailsComponent implements OnInit {
 
   product? : Product
 
-  constructor(private shopService : ShopService , private activatedRoute : ActivatedRoute){}
+  constructor(private shopService : ShopService ,
+              private activatedRoute : ActivatedRoute ,
+              private breadcrumbService : BreadcrumbService
+    ){
+        this.breadcrumbService.set('@productDetails', ' ')
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -21,7 +28,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if(id) this.shopService.getProductDetail(+ id).subscribe({
-      next : product => this.product = product,
+      next : product => {
+        this.product = product,
+        this.breadcrumbService.set('@productDetails', product.name)
+      },
       error : error => console.log("Lỗi getDetail SP :", error),
     })
 
